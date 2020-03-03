78a4b4b19a47ff8bab7fca12d73d1e4b4593d3ba
sakshi2199 <sakshi.sharma3099@gmail.com>
2020-03-03 18:43:05 +0530

    Create sum of 2 nos.c

---
+++ sum of 2 nos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a number");
+    scanf("%d", &a);
+    printf("Enter a number")l
+    scanf("%d", &b);
+    c = a+b;
+    printf("Sum = %d", c);
+    return(0);
+}
